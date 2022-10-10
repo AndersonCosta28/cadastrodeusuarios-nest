cd5ffd16097cbcb279d547bb1785293c90c1d57e
Anderson da Silva Costa <Andgtsc@Gmail.com>
2022-10-10 09:29:25 -0300

    Second commit

--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,6 +1,19 @@
+import { IsString, IsAlpha, IsEmail, IsPhoneNumber, IsIn, MaxLength, MinLength } from "class-validator";
+
 export class CreateUsuarioDto {
+    @IsString()
+    @MinLength(8)
     nome: string;
+
+    @IsEmail()
     email: string;
+
+    @IsPhoneNumber()
+    @MinLength(9)
+    @MaxLength(20)
     telefone: string;
+
+    @IsAlpha()
+    @IsIn(["Masculino", "Feminino"])
     sexo: string;
 }
